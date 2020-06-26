a36a81d50de8ce235266bda96cc376087c1d0274
Ali Youssefi <alyousse@microsoft.com>
2020-06-26 06:44:23 -0500

    commiting final for Custom Connector, AFN, and Flow Samples for App Insights

--- Samples/AzureFunction/MonitoringWithApplicationInsights/AzureFunctionAndApplicationInsights/MonitorWithApplicationInsights.CustomConnector.cs
+++ Samples/AzureFunction/MonitoringWithApplicationInsights/AzureFunctionAndApplicationInsights/MonitorWithApplicationInsights.CustomConnector.cs
@@ -1,3 +1,13 @@
+/*
+# This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. 
+# THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+# We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that. 
+# You agree: 
+# (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded; 
+# (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; 
+# and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code 
+*/
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -41,7 +51,7 @@ namespace Dynamics365.Monitoring.AzureFunctions
             {
                 foreach (var header in req.Headers)
                 {
-                    log.LogInformation(header.Key, header.Value);
+                    //log.LogInformation(header.Key, header.Value);
                 }
 
                 var content = req.Content;
@@ -64,8 +74,6 @@ namespace Dynamics365.Monitoring.AzureFunctions
         }
         #endregion
         #region event
-
-
         [FunctionName("TrackEvent")]
         public async Task<HttpResponseMessage> TrackEvent(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, ExecutionContext exCtx,
@@ -76,16 +84,40 @@ namespace Dynamics365.Monitoring.AzureFunctions
             {
                 foreach (var header in req.Headers)
                 {
-                    log.LogInformation(header.Key, header.Value);
+                    //log.LogInformation(header.Key, header.Value);
                 }
 
                 var content = req.Content;
                 string jsonContent = content.ReadAsStringAsync().Result;
                 dynamic data = JsonConvert.DeserializeObject(jsonContent);
-                var test = data?.name;
+                string correlationId = data?.correlationid;
+                var name = data?.name;
+                var duration = data?.duration;
+                var dependencyId = data?.id;
+                var actionTarget = data?.actionName;
+                var Type = data?.type;
+                var sequence = data?.sequence;
 
-                var properties = JsonConvert.DeserializeObject<Dictionary<string, string>>(data?.properties);
-                telemetryClient.TrackEvent("MonitorWithApplicationInsightsExample Function ended. Returning " + $"Hello, {test}");
+                string customDimensions = JsonConvert.SerializeObject(data?.properties);
+
+                EventTelemetry eventTelemetry = new EventTelemetry();
+                eventTelemetry.Name = name;
+                eventTelemetry.Sequence = sequence;
+
+                var d = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(customDimensions);
+
+                foreach (var key in d.Keys)
+                {
+                    // check if the value is not null or empty.
+                    if (!string.IsNullOrEmpty(d[key]))
+                    {
+                        var value = d[key];
+                        // code to do something with 
+                        eventTelemetry.Properties.Add(key, value);
+                    }
+                }
+
+                telemetryClient.TrackEvent(eventTelemetry);
 
                 RequestModel objRequestModel = new RequestModel();
                 objRequestModel.name = data.name;
@@ -137,6 +169,10 @@ namespace Dynamics365.Monitoring.AzureFunctions
         }
         #endregion
         #region exception
+        private class error {
+            public string code { get; set; }
+            public string message { get; set; }
+        }
         [FunctionName("TrackException")]
         public async Task<HttpResponseMessage> TrackException(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, ExecutionContext exCtx,
@@ -147,14 +183,39 @@ namespace Dynamics365.Monitoring.AzureFunctions
             {
                 foreach (var header in req.Headers)
                 {
-                    log.LogInformation(header.Key, header.Value);
+                    //log.LogInformation(header.Key, header.Value);
                 }
 
                 var content = req.Content;
                 string jsonContent = content.ReadAsStringAsync().Result;
                 dynamic data = JsonConvert.DeserializeObject(jsonContent);
                 var test = data?.name;
+                var correlationId = data?.correlationid;
+                string exceptionMessage = data?.exceptionMessage;
+
+                string workflowName = data?.workflowName;
+                string environmentName = data?.environmentName;
+                //Run Info
+                string workflowRunName = data?.workflowRunName;
+                string actionName = data?.actionName;
+                string severityLevel = data?.severityLevel;
+                string exceptionCode = data?.exceptionCode;
+                //Newtonsoft.Json.Linq.JObject error = data?.error;
+                //error workflowDataData = error.ToObject<error>();
+                //var workflowData = data?.workflow;
+                //error workflowDataData = JsonConvert.DeserializeObject<error>(error);
                 ExceptionTelemetry exceptionTelemetry = new ExceptionTelemetry();
+                //SeverityLevel
+                //Critical, Error, informational, warning, verbose
+                exceptionTelemetry.SeverityLevel = SeverityLevel.Critical;
+
+
+                exceptionTelemetry.Exception = new Exception(exceptionMessage);
+                exceptionTelemetry.ProblemId = exceptionCode;
+                exceptionTelemetry.Message = exceptionMessage;
+                StackFrame stackFrame = new StackFrame(workflowName, workflowRunName, 0, 0, actionName);
+
+                //Send Exception
                 telemetryClient.TrackException(exceptionTelemetry);
 
                 RequestModel objRequestModel = new RequestModel();
@@ -181,18 +242,35 @@ namespace Dynamics365.Monitoring.AzureFunctions
             {
                 foreach (var header in req.Headers)
                 {
-                    log.LogInformation(header.Key, header.Value);
+                    //log.LogInformation(header.Key, header.Value);
                 }
 
                 var content = req.Content;
                 string jsonContent = content.ReadAsStringAsync().Result;
                 dynamic data = JsonConvert.DeserializeObject(jsonContent);
-                var test = data?.name;
-                //telemetryClient.TrackDependency("MonitorWithApplicationInsightsExample Function ended. Returning " + $"Hello, {test}");
+                string correlationId = data?.correlationid;
+                var name = data?.name;
+                var duration = data?.duration;
+                var dependencyId = data?.id;
+                var actionTarget = data?.actionName;
+                var Type = data?.type;
+
+                DependencyTelemetry dependencyTelemetry = new DependencyTelemetry();
+                dependencyTelemetry.Data = name;
+                dependencyTelemetry.Duration = duration;
+                dependencyTelemetry.Id = dependencyId;
+                dependencyTelemetry.Name = name;
+                dependencyTelemetry.Target = actionTarget;
+                dependencyTelemetry.Type = Type;
+                telemetryClient.TrackDependency(dependencyTelemetry);
 
                 RequestModel objRequestModel = new RequestModel();
-
+                objRequestModel.success = true;
+                objRequestModel.correlationid = correlationId;
+                objRequestModel.eventType = Type;
+                objRequestModel.name = name;
                 HttpResponseMessage rtnObject = req.CreateResponse(HttpStatusCode.OK, objRequestModel);
+                
                 rtnObject.Headers.Add("InvocationId", exCtx.InvocationId.ToString());
                 return rtnObject;
             }
@@ -214,22 +292,22 @@ namespace Dynamics365.Monitoring.AzureFunctions
             {
                 foreach (var header in req.Headers)
                 {
-                    log.LogInformation(header.Key, header.Value);
+                    //log.LogInformation(header.Key, header.Value);
                 }
 
                 var content = req.Content;
                 string jsonContent = content.ReadAsStringAsync().Result;
                 dynamic data = JsonConvert.DeserializeObject(jsonContent);
-                var test = data?.name;
-                DateTimeOffset timestamp = data?.timestamp;
+                string test = data?.name;
+                DateTimeOffset timestamp = DateTime.Now;
                 TimeSpan duration = data?.duration;
                 string responseCode = data?.responseCode;
                 bool success = data?.success;
-
-                telemetryClient.TrackRequest("", timestamp, duration, responseCode, success);
+                log.LogInformation(exCtx.InvocationId.ToString() + " with name " + test);
+                telemetryClient.TrackRequest(test, timestamp, duration, responseCode, success);
 
                 RequestModel objRequestModel = new RequestModel();
-
+                objRequestModel.correlationid = data?.correlationId ?? exCtx.InvocationId.ToString();
                 HttpResponseMessage rtnObject = req.CreateResponse(HttpStatusCode.OK, objRequestModel);
                 rtnObject.Headers.Add("InvocationId", exCtx.InvocationId.ToString());
                 return rtnObject;
@@ -243,6 +321,7 @@ namespace Dynamics365.Monitoring.AzureFunctions
         #endregion
         public class RequestModel
         {
+            public string correlationid { get; set; }
             public string name { get; set; }
             public string eventType { get; set; }
             public bool success { get; set; }
--- Samples/AzureFunction/MonitoringWithApplicationInsights/UnitTestProject1/UnitTest1.cs
+++ Samples/AzureFunction/MonitoringWithApplicationInsights/UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,18 @@
+/*
+# This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. 
+# THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+# We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that. 
+# You agree: 
+# (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded; 
+# (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; 
+# and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code 
+*/
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -8,7 +22,42 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            //string request = @"{{""correlationid"": ""testcorrelation""",""name"": ""test app insight","properties": {"user": "Ali","userTwo": "Leslie"}}}";
+           
+            string jsonContent = "{\n\t\"correlationid\":\"testcorrelation\",\n\t\"name\": \"test app insight\",\n\t\"properties\":\n\t\t{\n\t\t\t\"user\":\"Ali\",\n\t\t\t\"userTwo\":\"John\"\n\t\t}\n}\n\n";
+            dynamic data = JsonConvert.DeserializeObject(jsonContent);
+            var name = data?.name;
+            var sequence = data?.sequence;
+            var dimensions = data?.dimensions;
+            var metrics = data?.metrics;
+            string customDimensions = JsonConvert.SerializeObject(data?.properties);
+            //data?.properties
+            dynamic properties = JsonConvert.DeserializeObject(data?.properties);
+        }
+
+        [TestMethod]
+        public void TestCustomDimensions()
+        {
+            string properties = "{\"user\":\"Ali\",\"userTwo\":\"John\"}";
+            EventTelemetry eventTelemetry = new EventTelemetry();
+            eventTelemetry.Name = "From Unit Test";
+            eventTelemetry.Sequence = "1";
+            var d = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(properties);
+
+            foreach (var key in d.Keys)
+            {
+                // check if the value is not null or empty.
+                if (!string.IsNullOrEmpty(d[key]))
+                {
+                    var value = d[key];
+                    // code to do something with 
+                    eventTelemetry.Properties.Add(key, value);
+                }
+            }
+            
+
+            TelemetryClient telemetryClient = new TelemetryClient();
+            telemetryClient.InstrumentationKey = "";
+            telemetryClient.TrackEvent(eventTelemetry);
         }
     }
 }
